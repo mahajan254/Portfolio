4a745afa771af89f148ee777e2c66d0b6eb37c98
mahajan254 <153797466+mahajan254@users.noreply.github.com>
2024-10-01 14:56:09 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,27 @@
+import React,{useEffect} from "react"
+import Home from "./components/Home"
+import Navbar from "./components/Navbar"
+import Skills from "./components/Skills"
+import Project from "./components/Project"
+import Aos from "aos";
+import "aos/dist/aos.css"
+import Contact from "./components/Contact"
+const App = () => {
+  useEffect(() => {
+    Aos.init();
+  
+   }, [])
+  return (
+    <div>
+     <Navbar/>
+     <div className="container_home">
+      <Home/>
+      <Skills/>
+      <Project/>
+      <Contact/>
+     </div>
+    </div>
+  )
+}
+
+export default App
